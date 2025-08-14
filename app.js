9bde2c330b82757cbba2243eb2261daea5ae370b
Erinaldo <erinaldo2005@gmail.com>
2025-08-14 01:38:13 -0300

    Adequação da frase ao tentar adicionar um amigo, com o campo vazio.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
-// Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let listaNomes = [];
 
@@ -7,11 +6,11 @@ function adicionarAmigo(){
     let nomeAmigo = document.getElementById('amigo').value;
     
     if (nomeAmigo == ''){
-        alert("Preencha com um nome!")
+        alert("Por favor, insira um nome.")
     } else {
         listaNomes.push(nomeAmigo);
         alert(`${nomeAmigo} adicionado!`)
-        
+
         document.getElementById('amigo').value = '';
     }
 }
