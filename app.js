5fb42138bb4cad741a61fbd40031792de40485b6
Erinaldo <erinaldo2005@gmail.com>
2025-08-14 01:30:28 -0300

    Tarefa: Adicionar amigo na lista

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaNomes = [];
+
+function adicionarAmigo(){
+    let nomeAmigo = document.getElementById('amigo').value;
+    
+    if (nomeAmigo == ''){
+        alert("Preencha com um nome!")
+    } else {
+        listaNomes.push(nomeAmigo);
+        alert(`${nomeAmigo} adicionado!`)
+        
+        document.getElementById('amigo').value = '';
+    }
+}
+
+
+
